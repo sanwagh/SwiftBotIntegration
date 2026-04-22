fa7a5b0bdf3115d1c7617e92dbeb00a574ea6b55
Sanat Wagh <sanatwagh0@gmail.com>
2026-04-22 23:14:15 +0100

    Initialisation

---
+++ src/main/java/com/SwitBotPersonalProj/SwiftBot.java
@@ -0,0 +1,8 @@
+package com.SwitBotPersonalProj;
+import swiftbot.*;
+
+public class SwiftBot {
+    // Use a package for each task
+    // Call the package and make sure each package has an entry method and one method that calls the entry method (could be a main method or init())
+    // Have a UI Handler
+}
